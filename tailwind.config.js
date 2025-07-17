fce9a5f348edc3efc9f7e5f6ed73071e127e93fe
mayarchit <agdivinity.cc@gmail.com>
2025-07-18 02:56:41 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./globals.css", // Include your CSS file if relevant
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
